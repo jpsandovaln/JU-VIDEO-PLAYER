096be5f056fbf3f26118bcc73be20e1ec7287843
FernandezRobert <soniic9911@gmail.com>
2022-10-11 13:20:22 -0400

    Fixed some package names

--- app/src/main/java/ju/video/player/view/information/search/SearchVideoPanel.java
+++ app/src/main/java/ju/video/player/view/information/search/SearchVideoPanel.java
@@ -1,9 +1,5 @@
 package ju.video.player.view.information.search;
 
-/*import main.java.ju.video.player.view.information.search.FolderPanel;
-import main.java.ju.video.player.view.information.search.FormatPanel;
-import main.java.ju.video.player.view.information.search.SearchPanel;*/
-
 import javax.swing.*;
 
 public class SearchVideoPanel extends JPanel {
