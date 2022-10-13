53f3dd9d1a776eb9ae9b5de2a6162e75daf6bfeb
Rodrigo Bernal <Rodrigo.Bernal@jala.university>
2022-10-13 16:50:38 -0400

    Add mediaPlayerComponent in the frame in the VideoPlayer class

--- app/src/main/java/ju/video/player/videoplayer/VideoPlayer.java
+++ app/src/main/java/ju/video/player/videoplayer/VideoPlayer.java
@@ -2,15 +2,25 @@ package ju.video.player.videoplayer;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.base.MediaPlayer;
+import uk.co.caprica.vlcj.player.component.EmbeddedMediaListPlayerComponent;
 
 public class VideoPlayer extends JFrame {
    private static final long serialVersionUID = 1L;
    private static final String TITLE = "Media Player";
 
+   MediaPlayerFactory mediaPlayerFactory = new MediaPlayerFactory();
+   private static EmbeddedMediaListPlayerComponent mediaPlayerComponent;
+
    public VideoPlayer(String title) {
       super(title);
+      mediaPlayerComponent = new EmbeddedMediaListPlayerComponent();
       initialize();
       setVisible(true);
    }
@@ -22,9 +32,15 @@ public class VideoPlayer extends JFrame {
       this.addWindowListener(new WindowAdapter() {
          @Override
          public void windowClosing(WindowEvent e) {
+            mediaPlayerComponent.release();
             System.exit(0);
          }
       });
+      JPanel contentPane = new JPanel();
+      contentPane.setLayout(new BorderLayout());
+      contentPane.add(mediaPlayerComponent, BorderLayout.CENTER);
+      this.setContentPane(contentPane);
+      this.setVisible(true);
    }
 
    public static void main(String[] args) {
