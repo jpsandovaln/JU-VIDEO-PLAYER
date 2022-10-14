3478696a89d0efe7beba3e29b1a1b0e90b70798c
Sergio Lema <sergio.lema@jala.university>
2022-10-13 21:33:16 -0400

    Join the media player with the UI first part

--- app/src/main/java/ju/video/player/view/videoplayer/VideoPlayerComponents/ControlButtons/PauseButton.java
+++ app/src/main/java/ju/video/player/view/videoplayer/VideoPlayerComponents/ControlButtons/PauseButton.java
@@ -6,11 +6,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 
-public class PauseButton extends JButton{
-    public PauseButton(ActionListener listener){
+public class PauseButton extends JButton {
+    public PauseButton(ActionListener listener) {
         setLocation(CENTER, CENTER);
-        setPreferredSize(new Dimension(30,30));
-        ImageIcon newIcon = new ImageIcon("app/src/main/java/ju/video/player/src/pause.png");
+        setPreferredSize(new Dimension(30, 30));
+        ImageIcon newIcon = new ImageIcon("JU-VIDEO-PLAYER/app/src/main/java/ju/video/player/src/pause.png");
         setIcon(newIcon);
         addActionListener(listener);
     }
--- app/src/main/java/ju/video/player/view/videoplayer/VideoPlayerPanel.java
+++ app/src/main/java/ju/video/player/view/videoplayer/VideoPlayerPanel.java
@@ -2,16 +2,20 @@ package ju.video.player.view.videoplayer;
 
 import javax.swing.*;
 import java.awt.*;
+
+import ju.video.player.view.MainFrame;
 import ju.video.player.view.videoplayer.VideoPlayerComponents.ControlButtonsGroup;
 import ju.video.player.view.videoplayer.VideoPlayerComponents.NameVideoText;
 import ju.video.player.view.videoplayer.VideoPlayerComponents.VideoFrame;
 
 public class VideoPlayerPanel extends JPanel {
+    MainFrame mainFrame;
 
-    public VideoPlayerPanel() {
+    public VideoPlayerPanel(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
         NameVideoText nameVideoText = new NameVideoText();
-        VideoFrame videoFrame = new VideoFrame();
-        ControlButtonsGroup controlButtonsGroup = new ControlButtonsGroup();
+        VideoFrame videoFrame = new VideoFrame(mainFrame);
+        ControlButtonsGroup controlButtonsGroup = new ControlButtonsGroup(videoFrame.getMediaPlayer());
         BorderLayout borderLayout = new BorderLayout(3, 3);
         setLayout(borderLayout);
         add(nameVideoText, BorderLayout.PAGE_START);
