7227d34054054039b0d7c1df33a6270c2d514e3e
Adriana Olivera <Adriana.Olivera@jala.university>
2022-10-28 08:33:36 -0400

    Add date picker to the filters

--- app/src/main/java/ju/video/player/view/playlist/filterspanel/DateComponentsPanel.java
+++ app/src/main/java/ju/video/player/view/playlist/filterspanel/DateComponentsPanel.java
@@ -17,12 +17,12 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.text.MaskFormatter;
+import javax.swing.JTextField;
 
-import ju.video.player.view.commons.Colors;
-import ju.video.player.view.commons.TextField;
+import ju.video.player.controller.DatePickerController;
 import ju.video.player.view.materialDesing.constants.Constants;
 
 public class DateComponentsPanel extends JPanel {
@@ -30,16 +30,26 @@ public class DateComponentsPanel extends JPanel {
     public DateComponentsPanel(String title) throws ParseException {
         initPanel();
         add(Box.createRigidArea(new Dimension(10, 0)));
-        MaskFormatter mask = new MaskFormatter("##-##-####");
+        /*MaskFormatter mask = new MaskFormatter("##-##-####");
         TextField field = new TextField(title, mask);
         field.setBackground(Colors.SECONDARY_BACKGROUNG_COLOR);
-        add(field);
+        add(field);*/
+        //JLabel label = new JLabel("Selected Date:");
+		final JTextField text = new JTextField(20);
+		JButton b = new JButton("popup");
+		//add(label);
+		add(text);
+		add(b);
+
+		b.addActionListener(new DatePickerController(this, text));
         add(Box.createRigidArea(new Dimension(10, 0)));
         JLabel calendarIcon = new JLabel();
 		ImageIcon image = new ImageIcon(Constants.RESOURCES_DIRECTORY + "/components/icons/calendar-icon.png");
-        Icon icon = new ImageIcon(image.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
+        Icon icon = new ImageIcon(image.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT));
         calendarIcon.setIcon(icon);
         add(calendarIcon);
+        //calendarIcon.addMouseListener(new CalendarIconController(field));
+		
         add(Box.createRigidArea(new Dimension(10, 0)));
         
     }
