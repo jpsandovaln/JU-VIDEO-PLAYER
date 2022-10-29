5f6a9b9d89ef1c292123ded7cc79d2ee844d0d8c
Alvaro Sivila <alvaro.sivila@jala.university>
2022-10-29 10:18:00 -0400

    Deleted unnecessary commented lines of code

--- app/src/main/java/ju/video/player/controller/ConvertController.java
+++ app/src/main/java/ju/video/player/controller/ConvertController.java
@@ -16,23 +16,12 @@ public class ConvertController implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // if (e.getSource() == convertFrame.getButton()) {
-        // convertFrame.getContentPane().removeAll();
-        // convertFrame.repaint();
         System.out.println("Pressed Convert");
-
-        // System.out.println("getname" +
-        // convert.getName("D:\\videoplayer\\JU-VIDEO-PLAYER\\VID_20220521_212816.mp4"));
         try {
             ConvertFile convertFile = new ConvertFile("C:\\Users\\alvar\\Desktop\\Videos\\VID_20220521_212249.mp4", "avi");
         } catch (IOException e1) {
             e1.printStackTrace();
         }
-        // convertFrame.addNewComponents();
-        // }
-        // convertFrame.setLabels("Game players: " +
-        // convertFrame.getChoice().getSelectedItem().toString());
-        // convertFrame.setVisible(false);
-        // new BoardFrame(convertFrame.getChoice().getSelectedItem().toString());
+        
     }
 }
