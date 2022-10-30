fae9996c8a9aa90a7107709ad73175a82f177e9f
Sergio Lema <sergio.lema@jala.university>
2022-10-30 10:02:07 -0400

    modify test file

--- app/src/test/java/ju/video/player/view/playlist/filterspanel/DataComponentsPanelTest.java
+++ app/src/test/java/ju/video/player/view/playlist/filterspanel/DataComponentsPanelTest.java
@@ -3,7 +3,6 @@ package ju.video.player.view.playlist.filterspanel;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import java.text.ParseException;
 
